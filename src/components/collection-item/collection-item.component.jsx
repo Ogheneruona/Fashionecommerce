9829928f4179ae273b2e4f452b7577ad9c84e5c5
Ogheneruona <ruon85@gmail.com>
2020-01-02 23:03:33 +0000

    added checkout page, checkout item component and updated cart to use selectors

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,7 +7,7 @@ import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
-	const { name, price, imageUrl } = item;
+	const { name, price, imageUrl } = item;	
 	return (
 	<div className='collection-item'>
 		<div
@@ -20,11 +20,12 @@ const CollectionItem = ({ item, addItem }) => {
 			<span className='name'>{name}</span>
 			<span className='price'>{price}</span>
 		</div>
-		<CustomButton onClick={() => addItem(item)} inverted > 
+		<CustomButton onClick={() => addItem(item)} inverted> 
 			Add to cart 
 		</CustomButton>
 	</div>  
-)};
+	);
+};
 
 const mapDispatchToProps = dispatch => ({
 	addItem: item => dispatch(addItem(item))
