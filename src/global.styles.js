42d146569c5dd2a48c2c8f6e46ed35f369de032b
Ogheneruona <ruon85@gmail.com>
2020-01-16 23:28:23 +0000

    optimizing for mobile 01

---
+++ src/global.styles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+body {
+	font-family: 'Open Sans Codensed';
+    padding: 20px 60px;
+
+//if app is below this width, render the instruction below.    
+    @medias screen and (max-width 800px) {
+        padding: 10px;
+    }
+}
+
+a {
+	text-decoration: none;
+	color: black;
+}
+
+/*this universal selector below enables us to make every element have a universal box-sizing to be border box*/
+* {
+	box-sizing: border-box;
+}
+`;
